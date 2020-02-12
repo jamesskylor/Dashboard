5e4022eab00ff4e7f343562d2e664ac88e0f3d31
Usr <j46xue@edu.uwaterloo.ca>
2020-02-12 14:24:03 -0500

    JS's initiated

---
+++ Profile/js/company.js
@@ -0,0 +1,18 @@
+document.getElementById("theSaveButton").addEventListener("click", (e)=>{
+    e.preventDefault();
+    saveInfo();
+});
+
+function saveInfo(){
+    // Take in the values
+    
+    // Check for validity/process information
+    
+    // If not valid
+    
+        // Reject and resubmit
+    
+    // If valid
+    
+        // Send data to the server
+}
---
+++ Profile/js/profile.js
@@ -0,0 +1,17 @@
+document.getElementById("theEditButton").addEventListener("click", (e)=>{
+    e.preventDefault();
+    goToEdit();
+});
+
+document.getElementById("theSaveButton").addEventListener("click", (e)=>{
+    e.preventDefault();
+    saveInfo();
+});
+
+function goToEdit(){
+    // Navigate to the Edit Profile Page
+}
+
+function saveinfo(){
+    
+}
