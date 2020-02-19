a8ae7b557282de2effa32b67d6576a3dbff7af11
Jackie <jackieliu191@gmail.com>
2020-02-19 11:38:12 -0500

    commit

--- db/index.js
+++ db/index.js
@@ -1,44 +1,26 @@
-const { Pool } = require('pg');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
 
-const pool = new Pool();
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
-module.exports = {
-    query: (text, params, callback) => {
-        const start = Date.now();
-        return pool.query(text, params, (err, res) => {
-            const duration = Date.now() - start;
-            console.log('executed query', { text, duration, rows: res.rowCount });
-            callback(err, res);
-        });
-    },
-    getClient: (callback) => {
-        pool.connect((err, client, done) => {
-            const query = client.query;
-            
-            // monkey patch the query method to keep track of the last query executed
-            client.query = (...args) => {
-                client.lastQuery = args;
-                return query.apply(client, args);
-            }
-            
-            // set a timeout of 5 seconds, after which we will log this client's last query
-            const timeout = setTimeout(() => {
-                console.error('A client has been checked out for more than 5 seconds!');
-                console.error(`The last executed query on this client was: ${client.lastQuery}`);
-            }, 5000);
-            
-            const release = (err) => {
-                // call the actual 'done' method, returning this client to the pool
-                done(err);
-                
-                // clear our timeout
-                clearTimeout(timeout);
-                
-                // set the query method back to its old un-monkey-patched version
-                client.query = query;
-            }
-            
-            callback(err, client, release);
-        })
-    }
-}
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
