f3ba00d9b79077dcd79fc4f04848f2e85583d49f
Jackie <jackieliu191@gmail.com>
2020-02-19 12:00:19 -0500

    commit

--- db/queries.js
+++ db/queries.js
@@ -42,3 +42,13 @@ module.exports = {
         })
     }
 }
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error,results) => {
+               
+              if (error) {
+                  throw error
+              } 
+        response.status(200).json(results.rows)
+    })
+}
