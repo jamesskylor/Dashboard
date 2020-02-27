81c9fd3fcc78a747562f0ed26e8e7dceccf78e60
Usr <j46xue@edu.uwaterloo.ca>
2020-02-27 15:54:00 -0500

    Made the sign up page look nice

---
+++ Login/createAccount.js
@@ -0,0 +1,68 @@
+document.getElementById("theForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    signup();
+});
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function signup() {
+    // Check password
+    
+    // Create the JSON
+    var newUser = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+        avatar: "placeholder"
+    }
+    // Connect to server
+    var url = 'https://dashdb.herokuapp.com';
+    var theAPIKey = "C@D@123";
+    let fetchData = async (url, theAPIKey, newUser) => {
+        let createID = await fetch(url+'/users', {
+            method: "POST",
+            headers: {
+                "apiKey": theAPIKey,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newUser)
+        })
+        .then(() => {
+            let getID = await fetch(url+'/id', {
+                method: "GET",
+                headers: {
+                    "apiKey": theAPIKey,
+                    "email": email,
+                    "key": password
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+            let tempIdNum = await getID.json();
+            if(tempIdNum[0].hasOwnProperty("id")) { // Check if login passed
+                let idNum = tempIdNum[0].id;
+
+                // put id into cookie that expires in a day
+                setCookie("dashId", idNum, 1);
+                location.assign("../Homepage/dashboard-index.html"); // Not sure if this is the right address
+            }
+            else {
+                // Error message as login failed
+                document.getElementById("invalidlogin").innerHTML = "Invalid Login";
+                document.getElementById("email").value = "";
+                document.getElementById("password").value = "";
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        return;
+    }
+    fetchData(url, theAPIKey, newUser);
+}
